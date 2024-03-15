9c340dbdfe8ef2e75de9e1b9ab5043f70ada963e
robertkleinschuster <github@robs.social>
2024-03-15 21:08:26 +0100

    update meta title

--- vite.config.ts
+++ vite.config.ts
@@ -10,22 +10,10 @@ export default defineConfig({
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png', '/public/**/*'],
       injectRegister: "script",
       manifest: {
-        // your manifest settings
-        name: 'My App',
-        short_name: 'MyApp',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
+        name: 'OeVA Beta',
+        short_name: 'OeVA Beta',
+        theme_color: '#333131',
+        icons: [],
       },
       workbox: {
         // workbox options for more advanced scenarios
