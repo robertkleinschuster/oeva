4279e7260954af629f374e7200f62adf096ec4b6
robertkleinschuster <github@robs.social>
2024-03-23 19:27:27 +0100

    disable button when feed in progress

--- src/components/FeedSheet.tsx
+++ src/components/FeedSheet.tsx
@@ -5,16 +5,30 @@ import axios from "axios";
 import {useLiveQuery} from "dexie-react-hooks";
 import {FeedStatus} from "./FeedStatus.tsx";
 import {feedDb} from "../db/FeedDb.ts";
+import {TransitFeedStatus} from "../db/Feed.ts";
 
-export const FeedSheet = ({feedId, onSheetClosed}: { feedId: number | null, onSheetClosed: () => void}) => {
+export const FeedSheet = ({feedId, onSheetClosed}: { feedId: number | null, onSheetClosed: () => void }) => {
     const dataImporter = new FeedImporter(feedDb, transitDB, axios)
     const feed = useLiveQuery(() => feedId ? feedDb.transit.get(feedId) : undefined, [feedId])
 
-    const stops = useLiveQuery(() => feedId ? feedDb.dependency.where({feed: 'transit', feed_id: feedId, table: 'stops'}).count() : 0, [feedId]);
-    const routes = useLiveQuery(() => feedId ? feedDb.dependency.where({feed: 'transit', feed_id: feedId, table: 'routes'}).count() : 0, [feedId]);
-    const trips = useLiveQuery(() => feedId ? feedDb.dependency.where({feed: 'transit', feed_id: feedId, table: 'trips'}).count() : 0, [feedId]);
+    const stops = useLiveQuery(() => feedId ? feedDb.dependency.where({
+        feed: 'transit',
+        feed_id: feedId,
+        table: 'stops'
+    }).count() : 0, [feedId]);
+    const routes = useLiveQuery(() => feedId ? feedDb.dependency.where({
+        feed: 'transit',
+        feed_id: feedId,
+        table: 'routes'
+    }).count() : 0, [feedId]);
+    const trips = useLiveQuery(() => feedId ? feedDb.dependency.where({
+        feed: 'transit',
+        feed_id: feedId,
+        table: 'trips'
+    }).count() : 0, [feedId]);
 
-    return <Sheet push backdrop closeByBackdropClick opened={Boolean(feed)} onSheetClosed={onSheetClosed} style={{height: "auto"}}>
+    return <Sheet push backdrop closeByBackdropClick opened={Boolean(feed)} onSheetClosed={onSheetClosed}
+                  style={{height: "auto"}}>
         {feed ? <PageContent>
                 <BlockTitle large>{feed.name}</BlockTitle>
                 <Block>
@@ -26,29 +40,30 @@ export const FeedSheet = ({feedId, onSheetClosed}: { feedId: number | null, onSh
                     <List outline>
                         <ListItem>
                             <span>Verwendet IFOPT</span>
-                            <Toggle checked={feed.is_ifopt} onChange={() => {
+                            <Toggle disabled={feed.status !== TransitFeedStatus.DONE} checked={feed.is_ifopt} onChange={() => {
                                 feedDb.transit.update(feed.id!, {
                                     is_ifopt: !feed.is_ifopt
                                 })
                             }}/>
                         </ListItem>
                     </List>
-                    <p>
-                        <Button color="red" onClick={async () => {
-                            feedDb.transit.delete(feed.id!)
-                        }}>Löschen</Button>
-                    </p>
-                    <p className="grid grid-cols-3 grid-gap">
-                        <Button onClick={async () => {
-                            await dataImporter.startDownload(feed.id!)
-                        }}>Herunterladen</Button>
-                        <Button onClick={async () => {
-                            await dataImporter.startImport(feed.id!)
-                        }}>Importieren</Button>
-                        <Button onClick={async () => {
-                            await dataImporter.startOptimize(feed.id!)
-                        }}>Optimieren</Button>
-                    </p>
+                        <p>
+                            <Button disabled={feed.status !== TransitFeedStatus.DONE} color="red" onClick={async () => {
+                                feedDb.transit.delete(feed.id!)
+                            }}>Löschen</Button>
+                        </p>
+                        <p className="grid grid-cols-3 grid-gap">
+                            <Button disabled={feed.status !== TransitFeedStatus.DONE} onClick={async () => {
+                                await dataImporter.startDownload(feed.id!)
+                            }}>Herunterladen</Button>
+                            <Button disabled={feed.status !== TransitFeedStatus.DONE} onClick={async () => {
+                                await dataImporter.startImport(feed.id!)
+                            }}>Importieren</Button>
+                            <Button disabled={feed.status !== TransitFeedStatus.DONE} onClick={async () => {
+                                await dataImporter.startOptimize(feed.id!)
+                            }}>Optimieren</Button>
+                        </p>
+
                 </Block>
             </PageContent> :
             <PageContent>
