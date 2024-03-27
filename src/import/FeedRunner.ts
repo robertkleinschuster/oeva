c7aa73b57d3325918393a11d9a1c86e3cbfa5c1d
robertkleinschuster <github@robs.social>
2024-03-27 21:20:36 +0100

    fix feed runner

--- src/import/FeedRunner.ts
+++ src/import/FeedRunner.ts
@@ -8,25 +8,24 @@ export class FeedRunner {
     static running: number | undefined
 
     run() {
-        if (!FeedRunner.running) {
-            feedDb.transit.where('status')
-                .noneOf([TransitFeedStatus.ERROR, TransitFeedStatus.DONE])
-                .each(feed => {
-                    if (FeedRunner.running === undefined || FeedRunner.running === feed.id) {
-                        FeedRunner.running = feed.id
-                        try {
-                            const dataImporter = new FeedImporter(feedDb, transitDB, axios)
-                            void dataImporter.run(feed.id!)
-                        } catch (error) {
-                            console.error(error)
-                            feedDb.transit.update(feed.id!, {
-                                status: TransitFeedStatus.ERROR,
-                                progress: error
-                            });
-                        }
-                        FeedRunner.running = undefined
+        feedDb.transit.where('status')
+            .noneOf([TransitFeedStatus.ERROR, TransitFeedStatus.DONE])
+            .each(async feed => {
+                if (FeedRunner.running === undefined) {
+                    FeedRunner.running = feed.id
+                    console.log('running', FeedRunner.running)
+                    try {
+                        const dataImporter = new FeedImporter(feedDb, transitDB, axios)
+                        await dataImporter.run(feed.id!)
+                    } catch (error) {
+                        console.error(error)
+                        feedDb.transit.update(feed.id!, {
+                            status: TransitFeedStatus.ERROR,
+                            progress: error
+                        });
                     }
-                })
-        }
+                    FeedRunner.running = undefined
+                }
+            })
     }
 }
