a6fb176c2ef2937c6a79eb618e1364d427aa0432
robertkleinschuster <github@robs.social>
2024-03-30 19:43:33 +0100

    enable swipe back

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import {createRoot} from 'react-dom/client';
 import App from './App';
 import {RunnerContextProvider} from "./RunnerContext";
 import {FeedRunner} from "./import/FeedRunner";
+import {preventBrowserHistorySwipeGestures} from "./block-swipe";
+preventBrowserHistorySwipeGestures()
 
 const runner = new FeedRunner();
 
