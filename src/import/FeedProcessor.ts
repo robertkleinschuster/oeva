4ddaf506025e14608dc8c8ca04646f4dac6b1edf
robertkleinschuster <github@robs.social>
2024-04-05 15:24:06 +0200

    improve station search

--- src/import/FeedProcessor.ts
+++ src/import/FeedProcessor.ts
@@ -1,10 +1,10 @@
 import {H3_RESOLUTION, Stopover} from "../db/Schedule";
 import {ScheduleDB} from "../db/ScheduleDB";
-import lunr from "lunr";
 import {FeedDB} from "../db/FeedDb";
 import {GTFSDB} from "../db/GTFSDB";
 import {createStopover} from "./StopoverFactory";
 import {latLngToCell} from "h3-js";
+import Tokenizer from "wink-tokenizer";
 
 export class FeedProcessor {
     private offset: number = 0
@@ -98,13 +98,15 @@ export class FeedProcessor {
         }, 1500);
 
         try {
+            const tokenizer = new Tokenizer()
+
             for (const stop of stops) {
                 await this.scheduleDb.station.put({
                     id: this.prefixId(feedId, stop.stop_id),
                     feed_id: feedId,
                     feed_station_id: stop.stop_id,
                     name: stop.stop_name,
-                    keywords: lunr.tokenizer(stop.stop_name).map(String),
+                    keywords: tokenizer.tokenize(stop.stop_name).map(token => token.value),
                     h3_cell: latLngToCell(stop.stop_lat, stop.stop_lon, H3_RESOLUTION),
                 })
                 this.offset++;
@@ -142,6 +144,8 @@ export class FeedProcessor {
             });
         }, 1500);
         try {
+            const tokenizer = new Tokenizer()
+
             for (const trip of trips) {
                 const route = await this.transitDb.routes.get(trip.route_id)
                 const service = await this.transitDb.calendar.get(trip.service_id)
@@ -158,7 +162,7 @@ export class FeedProcessor {
                         route_type: route.route_type,
                         name: trip.trip_short_name ? trip.trip_short_name : route.route_short_name,
                         direction: trip.trip_headsign ?? '',
-                        keywords: lunr.tokenizer(`${trip.trip_short_name} ${route.route_short_name}`).map(String),
+                        keywords: tokenizer.tokenize(`${trip.trip_short_name} ${route.route_short_name}`).map(token => token.value),
                         service,
                         exceptions: new Map(exceptions.map(exception => [exception.date, exception.exception_type])),
                     })
--- src/pages/StationSearch.tsx
+++ src/pages/StationSearch.tsx
@@ -13,8 +13,9 @@ import {
 } from '@ionic/react';
 import React, {useEffect, useState} from "react";
 import {scheduleDB} from "../db/ScheduleDB";
-import lunr from "lunr";
 import {Station} from "../db/Schedule";
+import Tokenizer from "wink-tokenizer";
+import Fuse from "fuse.js";
 
 const StationSearch: React.FC = () => {
     const [keyword, setKeyword] = useState('')
@@ -23,26 +24,17 @@ const StationSearch: React.FC = () => {
     useEffect(() => {
         setStations([])
         if (keyword.length > 1) {
-            const keywords = lunr.tokenizer(keyword).map(String)
+            const tokenizer = new Tokenizer
+            const keywords = tokenizer.tokenize(keyword).map(token => token.value)
             scheduleDB.station
                 .where('keywords')
-                .startsWithAnyOf(keywords)
+                .startsWithAnyOfIgnoreCase(keywords)
                 .toArray(stations => {
-                    const map = new Map<string, Station>
-                    for (const station of stations) {
-                        map.set(station.id, station)
-                    }
-                    setStations(Array.from(map.values()).sort((a, b) => {
-                        if (a.name.toLowerCase() === keyword.toLowerCase()) {
-                            return -1;
-                        }
-                        if (b.name.toLowerCase() === keyword.toLowerCase()) {
-                            return 1;
-                        }
-                        const aMatches = a.keywords.filter(k => keywords.filter(k2 => k.startsWith(k2)).length).length
-                        const bMatches = b.keywords.filter(k => keywords.filter(k2 => k.startsWith(k2)).length).length
-                        return bMatches - aMatches
-                    }))
+                    const fuse = new Fuse(stations, {
+                        keys: ['name'],
+                        threshold: 0.3
+                    })
+                    setStations(fuse.search(keyword).map(result => result.item))
                 })
         }
     }, [keyword]);
