8550477a98cb6ab5513274fbba82864eba98e32a
robertkleinschuster <github@robs.social>
2024-04-07 20:52:05 +0200

    add background audio hack

--- src/import/FeedRunner.ts
+++ src/import/FeedRunner.ts
@@ -9,6 +9,7 @@ import NoSleep from 'nosleep.js';
 export class FeedRunner {
     running: number | undefined
     private nosleep = new NoSleep()
+    private audio: HTMLAudioElement | undefined
 
     async run() {
         if (this.running === undefined) {
@@ -20,6 +21,7 @@ export class FeedRunner {
                 if (feed) {
                     this.running = feed.id
                     await this.nosleep.enable()
+                    this.backgroundExec();
                     try {
                         const dataImporter = new FeedImporter(feedDb, new GTFSDB(feed.id!), scheduleDB, axios)
                         await dataImporter.run(feed.id!)
@@ -36,14 +38,53 @@ export class FeedRunner {
                 console.log(e)
             }
             this.nosleep.disable()
+            this.audio?.pause()
             this.running = undefined
         }
 
         return new Promise<void>(resolve => {
-            setTimeout(async ()=> {
+            setTimeout(async () => {
                 await this.run()
                 resolve()
             }, 1000)
         })
+    }
+
+
+    backgroundExec() {
+        if (this.audio) {
+            void this.audio.play()
+            return;
+        }
+        // unlock audio context
+        let ctx = new AudioContext();
+
+        // create silent sound
+        let bufferSize = 2 * ctx.sampleRate,
+            emptyBuffer = ctx.createBuffer(1, bufferSize, ctx.sampleRate),
+            output = emptyBuffer.getChannelData(0);
+
+        // fill buffer
+        for (let i = 0; i < bufferSize; i++)
+            output[i] = 0;
+        // create source node
+        let source = ctx.createBufferSource();
+        source.buffer = emptyBuffer;
+        source.loop = true;
+
+        // create destination node
+        let node = ctx.createMediaStreamDestination();
+        source.connect(node);
+
+        // dummy audio element
+        let audio = document.createElement("audio");
+        audio.style.display = "none";
+        document.body.appendChild(audio);
+
+        // set source and play
+        audio.srcObject = node.stream;
+        this.audio = audio
+        void audio.play();
+        // background exec enabled
     }
 }
