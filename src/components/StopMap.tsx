7da3f626fe28d2cde82aea4c3b12de59e4a1612b
robertkleinschuster <github@robs.social>
2024-04-21 16:42:44 +0200

    test detect retina

--- src/components/StopMap.tsx
+++ src/components/StopMap.tsx
@@ -29,6 +29,7 @@ const StopMap: React.FC<{ cell: H3IndexInput, tooltip: string }> = ({cell, toolt
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                detectRetina
             />
             <Marker
                 position={position}
