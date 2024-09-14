b60bf8bddf1e8eef28afa463902d66a018ad8023
robertkleinschuster <git@robs.social>
2024-09-14 14:53:34 +0200

    install sqlocal and kysely

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,20 @@ import "vitest/config"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        headers: {
-            'Cross-Origin-Opener-Policy': 'same-origin',
-            'Cross-Origin-Embedder-Policy': 'require-corp',
-        },
-    },
     optimizeDeps: {
-        exclude: ['@sqlite.org/sqlite-wasm'],
+        exclude: ['sqlocal'],
     },
     plugins: [
+        {
+            name: 'configure-response-headers',
+            configureServer: (server) => {
+                server.middlewares.use((_req, res, next) => {
+                    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+                    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+                    next();
+                });
+            },
+        },
         react(),
         VitePWA({
             includeAssets: ['/public/**/*'],
